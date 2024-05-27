70e6997c82a87edae689e89fdcdd5f722a25a52c
Azuhonahus <sashanikonov1810@mail.ru>
2024-05-27 14:05:28 +0300

    commit

---
+++ hello_world.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    
+    getline(cin, name);
+    
+    
+    cout<<"Hello World from " << name;
+
+    return 0;
+}
